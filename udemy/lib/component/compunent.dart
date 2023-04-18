d376ed46229e2bc64f6214004444a529a4be4e0f
Youssef Abd-El-Ghfar Abd-El-Twab <eissay240@gmail.com>
2023-04-18 23:08:52 +0200

    udemy
    edit

--- udemy/lib/component/compunent.dart
+++ udemy/lib/component/compunent.dart
@@ -118,11 +118,103 @@ class Compunent extends StatelessWidget {
           
           Row(
             children: [
-              Container(
-                child: Image.asset("images/"),
-              )
+              Padding(
+                padding: const EdgeInsets.only(left: 20,right: 10),
+                child: Column(
+                  children: [
+
+                    Container(
+                      child: Image.asset(
+                          "images/d1.jpeg",
+                          scale: 2),
+                    ),
+
+                    Container(
+                      child: Text(
+                          "Design Course",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+
+                    Container(
+                      child: Text(
+                        "Trainee course",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                          height: 2,
+                        ),
+                      ),
+                    ),
+
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(right: 3,top: 5),
+                          child: Text(
+                            "4.8",
+                            style: TextStyle(
+                              color: Colors.yellow[900],
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.star,color: Colors.yellow[700],),
+                            Icon(Icons.star,color: Colors.yellow[700],),
+                            Icon(Icons.star,color: Colors.yellow[700],),
+                            Icon(Icons.star,color: Colors.yellow[700],),
+                          ],
+                        ),
+                        Container(
+                          child: Text(
+                              " (417)",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Container(
+                      child: Text(
+                        "EGP 387",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          height: 2,
+                        ),
+                      ),
+                    ),
+
+                    Container(
+                      color: Colors.yellow,
+                      width: 150,
+                      height: 30,
+                      margin: EdgeInsets.only(top: 5),
+                      child: Center(
+                        child: Text(
+                          "BestSeller",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            height: 1,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
-          )
+          ),
 
         ],
       ),
