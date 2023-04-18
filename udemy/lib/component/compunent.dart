a868daeabb10331269945233614046d5153d6180
Youssef Abd-El-Ghfar Abd-El-Twab <eissay240@gmail.com>
2023-04-18 21:02:02 +0200

    udemy
    edit

--- udemy/lib/component/compunent.dart
+++ udemy/lib/component/compunent.dart
@@ -8,7 +8,19 @@ class Compunent extends StatelessWidget {
     return Column(
       children: [
         Container(
-          margin: EdgeInsets.all(10),
+          color: Colors.deepPurpleAccent,
+          width: 400,
+          height: 30,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text("Future-Ready skills on your schedule"),
+              Icon(Icons.cancel_outlined),
+            ],
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.all(5),
           child: Image.asset('images/Hero Section - Mobile.jpg'),
         ),
       ],
