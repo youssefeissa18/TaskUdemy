b1528a8ddfebb82dc67da3e4c8187a0c89fafbb9
Youssef Abd-El-Ghfar Abd-El-Twab <eissay240@gmail.com>
2023-04-18 22:36:54 +0200

    udemy
    edit

--- udemy/lib/component/compunent.dart
+++ udemy/lib/component/compunent.dart
@@ -6,102 +6,126 @@ class Compunent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          color: Colors.deepPurpleAccent,
-          width: 400,
-          height: 30,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text("Future-Ready skills on your schedule"),
-              Icon(Icons.cancel_outlined),
-            ],
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.only(bottom: 10),
-          child: Image.asset('images/Hero Section - Mobile.jpg'),
-        ),
-
-        Container(
-          child: Text(
-            "Learning that fits",
-            style: TextStyle(
-              letterSpacing: 3,
-              fontSize: 36,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: [
+          Container(
+            color: Colors.deepPurpleAccent,
+            width: 400,
+            height: 30,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text("Future-Ready skills on your schedule"),
+                Icon(Icons.cancel_outlined),
+              ],
             ),
           ),
-        ),
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            child: Image.asset('images/Hero Section - Mobile.jpg'),
+          ),
 
-        Container(
-          alignment: Alignment.centerLeft,
-          margin: EdgeInsets.only(left: 32),
-          child: Text(
-              "Skills for your present (and future)",
+
+          Container(
+            child: Text(
+              "Learning that fits",
+              style: TextStyle(
+                letterSpacing: 3,
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+
+          Container(
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(left: 32),
+            child: Text(
+                "Skills for your present (and future)",
+                style: TextStyle(
+                  color: Colors.white,
+                  height: 1.5,
+                  fontSize: 18,
+                ),
+            ),
+          ),
+
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 20,right: 120),
+                child: Text(
+                  "Categories",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    height: 1.5
+                  ),
+                ),
+              ),
+              Container(
+                child: Text(
+                    "see all",
+                  style: TextStyle(
+                    color: Colors.deepPurple,
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Jobs("Development"),
+                Jobs("IT & Software"),
+                Jobs("Office Work"),
+                Jobs("Designer"),
+              ],
+            ),
+          ),
+
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Jobs("Business"),
+                Jobs("Finance & Accounting"),
+                Jobs("Programmer"),
+                Jobs("Analysis"),
+              ],
+            ),
+          ),
+
+          Container(
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(top: 30,left: 25,bottom: 20),
+            child: Text(
+                "Top Courses in Design",
               style: TextStyle(
                 color: Colors.white,
-                height: 1.5,
-                fontSize: 18,
-              ),
-          ),
-        ),
-
-        Row(
-          children: [
-            Container(
-              margin: EdgeInsets.only(left: 20,right: 120),
-              child: Text(
-                "Categories",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  height: 1.5
-                ),
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            Container(
-              child: Text(
-                  "see all",
-                style: TextStyle(
-                  color: Colors.deepPurple,
-                ),
-              ),
-            ),
-          ],
-        ),
-
-
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              Jobs("Development"),
-              Jobs("IT & Software"),
-              Jobs("Office Work"),
-              Jobs("Another Job"),
-            ],
           ),
-        ),
-
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
+          
+          Row(
             children: [
-              Jobs("Business"),
-              Jobs("Finance & Accounting"),
-              Jobs("Programmer"),
-              Jobs("Analysis"),
+              Container(
+                child: Image.asset("images/"),
+              )
             ],
-          ),
-        ),
+          )
 
-
-      ],
+        ],
+      ),
     );
   }
 }
