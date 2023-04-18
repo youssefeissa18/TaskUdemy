292b4d073ea6c397ae747e9ce6c7ec594d3d9933
Youssef Abd-El-Ghfar Abd-El-Twab <eissay240@gmail.com>
2023-04-18 19:52:37 +0200

    udemy
    edit

--- udemy/lib/home_screen/home_screen.dart
+++ udemy/lib/home_screen/home_screen.dart
@@ -7,21 +7,29 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.grey,
-        title: Container(
-          margin: EdgeInsets.symmetric(horizontal: 80),
-          child: Text(
-            "Udemy",
-            style: TextStyle(
-               fontWeight: FontWeight.bold,
-              color: Colors.black,
-              fontSize: 30,
+        backgroundColor: Colors.white38,
+        title: Row(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 75),
+              child: Text(
+                "Udemy",
+                style: TextStyle(
+                   fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 30,
+                ),
+              ),
             ),
-          ),
+
+            Icon(Icons.shopping_cart_outlined,color: Colors.black,),
+            Spacer(),
+            Icon(Icons.notifications_active_outlined,color: Colors.black,),
+          ],
         ),
       ),
       drawer: Drawer(
-        
+
       ),
 
 
