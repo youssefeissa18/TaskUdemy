31189b39c8513f186376ad93e2f0c0b00a98bae4
Youssef Abd-El-Ghfar Abd-El-Twab <eissay240@gmail.com>
2023-04-18 22:21:45 +0200

    udemy
    edit

--- udemy/lib/app_root.dart
+++ udemy/lib/app_root.dart
@@ -7,6 +7,7 @@ class AppRoot extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(
 
       ),
--- udemy/lib/component/compunent.dart
+++ udemy/lib/component/compunent.dart
@@ -99,6 +99,8 @@ class Compunent extends StatelessWidget {
             ],
           ),
         ),
+
+
       ],
     );
   }
