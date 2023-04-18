492bd28b1223a11ecb57a7df7c1a54299032ddc5
Youssef Abd-El-Ghfar Abd-El-Twab <eissay240@gmail.com>
2023-04-18 19:36:29 +0200

    udemy
    edit

--- udemy/lib/app_root.dart
+++ udemy/lib/app_root.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:udemy/home_screen/home_screen.dart';
 
 class AppRoot extends StatelessWidget {
   const AppRoot({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: HomeScreen(
+
+      ),
+    );
   }
 }
--- udemy/lib/home_screen/home_screen.dart
+++ udemy/lib/home_screen/home_screen.dart
@@ -5,6 +5,28 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: Container(
+          margin: EdgeInsets.symmetric(horizontal: 80),
+          child: Text(
+            "Udemy",
+            style: TextStyle(
+               fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 30,
+            ),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        
+      ),
+
+
+
+      body: Container(),
+    );
   }
 }
